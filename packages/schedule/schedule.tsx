528a4dcd17f098c8bba6bdfeeec2f1c2c208a39d
Baoguoq1ang <18701094030@163.com>
2022-05-21 23:33:16 +0800

    schedule组件页面布局

--- packages/schedule/schedule.tsx
+++ packages/schedule/schedule.tsx
@@ -26,6 +26,41 @@ export default defineComponent({
                 }
             })
 
+            const colspan = 24
+            const time = 48
+            const week = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日']
+
+            const thTime = () => {
+                const result = []
+                for (let i = 0; i < colspan; i++) {
+                    result.push(<td colspan={2}>{i}</td>)
+                }
+                return result
+            }
+
+            const timeNode = (weekData: number) => {
+                const result = []
+                for (let i = 0; i < 48; i++) {
+                    result.push(
+                        <td data-week={weekData} data-time={i}  class={`${prefixCls}-schedule-calendar-time`}></td>
+                    )
+                }
+                return result
+            }
+
+            const weekNode = () => {
+                const result = []
+                for (let i = 0; i < week.length; i++) {
+                    result.push(
+                        <tr>
+                            <td>{week[i]}</td>
+                            {timeNode(i)}
+                        </tr>
+                    )
+                }
+                return result
+            }
+
             const node = (
                 <div class={`${prefixCls}-schedule`}>
                     <div class={`${prefixCls}-schedule-header`}>
@@ -33,6 +68,30 @@ export default defineComponent({
                         <div class={`${prefixCls}-schedule-explain`}>
                             <div class={`${prefixCls}-schedule-h-selected`}>{ props.selectedText }</div>
                             <div class={`${prefixCls}-schedule-h-selectable`}>{ props.selectableText }</div>
+                        </div>
+                    </div>
+                    <div class={`${prefixCls}-schedule-calendar`}>
+                        <div class={`${prefixCls}-schedule-calendar-twrap`}>
+                            <table class={`${prefixCls}-schedule-calendar-table`}>
+                                <thead>
+                                    <tr>
+                                        <th rowspan="8" class="week-td">星期/时间</th>
+                                        <th colspan={colspan}>00:00 - 12:00</th>
+                                        <th colspan={colspan}>12:00 - 24:00</th>
+                                    </tr>
+                                    <tr>{ thTime() }</tr>
+                                </thead>
+                                <tbody>
+                                    { weekNode() }
+                                    <tr>
+                                        <td colspan="49" class={`${prefixCls}-schedule-calendar-tip`}>
+                                            <div class="clearfix">
+                                                <p>可拖动鼠标选择时间段</p>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
